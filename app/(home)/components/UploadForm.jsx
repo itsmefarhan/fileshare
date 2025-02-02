4264fee3a6e970bc390285e2600ed68b399a9ff2
Farhan Farooq <farhan.farooq@live.com>
2025-02-03 03:13:15 +0500

    upload data

--- app/(home)/components/UploadForm.jsx
+++ app/(home)/components/UploadForm.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { addDocAPI } from "@/app/endpoints/docs";
 import { useEffect, useState } from "react";
 
 const UploadForm = ({ edit }) => {
@@ -9,6 +10,15 @@ const UploadForm = ({ edit }) => {
       setVal({ title: edit.title, file: edit.file });
     }
   }, [edit]);
+
+  const handleSubmit = async () => {
+    if (edit) {
+    } else {
+      if (!val.title || !val.file) return;
+      await addDocAPI(val);
+      setVal({ title: "", file: "" });
+    }
+  };
   return (
     <div className="mt-10 border p-5 rounded-md">
       <h4 className="font-bold text-center">{edit ? "Edit" : "Upload"} File</h4>
@@ -25,7 +35,10 @@ const UploadForm = ({ edit }) => {
         onChange={(e) => setVal({ ...val, file: e.target.files[0] })}
       />
       <div className="text-center">
-        <button className="bg-slate-800 text-white py-1 px-2 rounded-md outline-none mt-2">
+        <button
+          className="bg-slate-800 text-white py-1 px-2 rounded-md outline-none mt-2"
+          onClick={handleSubmit}
+        >
           Submit
         </button>
       </div>
---
+++ app/endpoints/docs.js
@@ -0,0 +1,26 @@
+import { createClient } from "../utils/client";
+
+const supabase = createClient();
+const bucket = supabase.storage.from("files");
+
+const uploadFile = async (file) => {
+  const fileName = Date.now() + "-" + file.name;
+
+  const filePath = await bucket.upload(fileName, file);
+  const fileUrl = bucket.getPublicUrl(filePath.data.path);
+
+  return { file_path: filePath.data.path, file_url: fileUrl.data.publicUrl };
+};
+
+export const addDocAPI = async (val) => {
+  const res = await supabase.auth.getUser();
+
+  const { file_path, file_url } = await uploadFile(val.file);
+
+  await supabase.from("docs").insert({
+    title: val.title,
+    file_url,
+    file_path,
+    uid: res.data.user.id,
+  });
+};
